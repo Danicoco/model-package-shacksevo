751c0ed8f024243e4e15c80bc62429a03d10f8af
Aderemi Dare <richardjohn740@gmail.com>
2022-02-06 03:33:23 +0100

    changes imports

--- src/usability/index.ts
+++ src/usability/index.ts
@@ -1,2 +1,14 @@
-export * from './GameService';
-export * from './ErrorService';
+import ApiService from './ApiService'
+import ErrorService from './ErrorService'
+import GameRoundService from './GameRound'
+import GameService from './GameService'
+import PartnerService from './PartnerService';
+
+
+export {
+    ApiService,
+    GameService,
+    ErrorService,
+    PartnerService,
+    GameRoundService
+}
