d1cdd89cd2bff625314e6772fbe2d9aa335d0c83
Aderemi Dare <richardjohn740@gmail.com>
2022-02-10 16:36:24 +0100

    new version built

--- build/usability/index.d.ts
+++ build/usability/index.d.ts
@@ -1,10 +1,12 @@
 import ApiService from './ApiService';
 import GameService from './GameService';
+import RoleService from './RoleService';
 import UserService from './UserService';
 import ErrorService from './ErrorService';
+import AdminService from './AdminService';
 import GameRoundService from './GameRound';
 import WalletService from './WalletService';
 import BetPlacedService from './BetService';
 import PartnerService from './PartnerService';
-export { ApiService, GameService, UserService, ErrorService, WalletService, PartnerService, BetPlacedService, GameRoundService, };
+export { ApiService, GameService, UserService, RoleService, ErrorService, AdminService, WalletService, PartnerService, BetPlacedService, GameRoundService, };
 //# sourceMappingURL=index.d.ts.map
