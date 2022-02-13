ab509837b69e015d73cb9dcee5e205a032985088
Aderemi Dare <richardjohn740@gmail.com>
2022-02-13 05:14:40 +0100

    gameaccess service added

---
+++ src/usability/GameAccessService.ts
@@ -0,0 +1,58 @@
+import { GameAccess } from '../models';
+import { IGameAccess} from '../../types';
+
+
+class GameAccessService {
+    private partnerId: string | null;
+    private _id: string | null;
+
+    constructor(_id: string | null, partnerId: string | null) {
+        this._id = _id;
+        this.partnerId = partnerId;
+    }
+
+    public async create(params: Partial<IGameAccess>) {
+        try {
+            const gameAccess = new GameAccess({ ...params });
+            await gameAccess.save();
+            return gameAccess;
+        } catch (error: any) {
+            throw new Error(error.message)
+        }
+    }
+
+    public async findOne() {
+        const gameAccess = await GameAccess
+            .findOne()
+            .where(this._id ? '_id' : 'partnerId')
+            .equals(this._id ? this._id : this.partnerId)
+            .catch((e) => {
+                throw new Error(e.message);
+            });
+        return gameAccess;
+    }
+
+    public async findAll() {
+        const gameAccesss = await GameAccess
+            .find()
+            .catch((e) => {
+                throw new Error(e.message)
+            });
+        return gameAccesss;
+    }
+    
+    public async deleteOne() {
+        const gameAccess = await GameAccess
+            .findOneAndDelete()
+            .where(this._id ? "_id": "partnerId")
+            .equals(this._id ? this._id : this.partnerId)
+            .catch((e) => {
+                throw new Error(e.message);
+            });
+
+        return gameAccess;
+
+    }
+}
+
+export default GameAccessService;
