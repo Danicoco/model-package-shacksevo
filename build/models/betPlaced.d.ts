3042060df0958ceffa8001008a162b63fefe13d1
Aderemi Dare <richardjohn740@gmail.com>
2022-04-09 11:44:58 +0100

    bet placed retrieved

--- build/models/betPlaced.d.ts
+++ build/models/betPlaced.d.ts
@@ -1,3 +1,5 @@
-declare const _default: any;
+/// <reference types="mongoose" />
+import { IBetPlaced } from '../../types';
+declare const _default: import("mongoose").Model<IBetPlaced, {}, {}, {}>;
 export default _default;
 //# sourceMappingURL=betPlaced.d.ts.map
