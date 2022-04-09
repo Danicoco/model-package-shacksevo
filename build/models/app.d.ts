a5eca3eff03a670f0b04143ad04c34d852b64ab8
Aderemi Dare <richardjohn740@gmail.com>
2022-04-09 11:44:34 +0100

    app redundant

--- build/models/app.d.ts
+++ build/models/app.d.ts
@@ -1,3 +1,5 @@
-declare const _default: any;
+/// <reference types="mongoose" />
+import { IApp } from '../../types';
+declare const _default: import("mongoose").Model<IApp, {}, {}, {}>;
 export default _default;
 //# sourceMappingURL=app.d.ts.map
