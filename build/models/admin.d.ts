294cc19a7a55fc587a303bd76fabc6f41e1719c5
Aderemi Dare <richardjohn740@gmail.com>
2022-04-09 11:44:15 +0100

    admin util

--- build/models/admin.d.ts
+++ build/models/admin.d.ts
@@ -1,3 +1,5 @@
-declare const _default: any;
+/// <reference types="mongoose" />
+import { IAdmin } from '../../types';
+declare const _default: import("mongoose").Model<IAdmin, {}, {}, {}>;
 export default _default;
 //# sourceMappingURL=admin.d.ts.map
