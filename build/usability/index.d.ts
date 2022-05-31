3a4d4a111f4fcd99c42a3c23333d0a956ec65148
Aderemi Dare <richardjohn740@gmail.com>
2022-05-31 11:13:27 +0100

    chat service added

--- build/usability/index.d.ts
+++ build/usability/index.d.ts
@@ -3,6 +3,7 @@ import AppService from './AppService';
 import GameService from './GameService';
 import RoleService from './RoleService';
 import UserService from './UserService';
+import ChatService from './ChatService';
 import ErrorService from './ErrorService';
 import AdminService from './AdminService';
 import GameRoundService from './GameRound';
@@ -12,5 +13,5 @@ import PartnerService from './PartnerService';
 import GameAccessService from './GameAccessService';
 import PermissionService from './PermissionService';
 import TransactionService from './TransactionService';
-export { ApiService, AppService, GameService, UserService, RoleService, ErrorService, AdminService, WalletService, PartnerService, BetPlacedService, GameRoundService, GameAccessService, PermissionService, TransactionService, };
+export { ApiService, AppService, GameService, UserService, ChatService, RoleService, ErrorService, AdminService, WalletService, PartnerService, BetPlacedService, GameRoundService, GameAccessService, PermissionService, TransactionService, };
 //# sourceMappingURL=index.d.ts.map
