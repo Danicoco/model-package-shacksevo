8fb8c7de350038158535e3734f710e7c2beecca3
Onwordi Prince Alfred <princelini4@gmail.com>
2023-03-03 03:10:52 +0100

    updates

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,24 +1,25 @@
-import Rtp from './rtp';
-import API from './api';
-import APP from './app';
-import Game from './game';
-import User from './user';
-import Chat from './chat';
-import Role from './roles';
-import Admin from './admin';
-import Wallet from './wallet';
-import Reward from './reward';
-import Partner from './partner';
-import Feedback from './feedback';
-import ErrorMessage from './error';
-import GameRound from './gameRound';
-import BetPlaced from './betPlaced';
-import GameAccess from './gameAccess';
-import Permission from './permission';
-import Transaction from './transaction';
-import Leaderboard from './leaderboard';
-import Tornament from './tornamentPlayers';
-import TornamentPlayers from './tornamentPlayers'
+import Rtp from "./rtp";
+import API from "./api";
+import APP from "./app";
+import Game from "./game";
+import User from "./user";
+import Chat from "./chat";
+import Role from "./roles";
+import Admin from "./admin";
+import Wallet from "./wallet";
+import Reward from "./reward";
+import Partner from "./partner";
+import Feedback from "./feedback";
+import ErrorMessage from "./error";
+import GameRound from "./gameRound";
+import BetPlaced from "./betPlaced";
+import SpinRecord from "./spinRecord";
+import GameAccess from "./gameAccess";
+import Permission from "./permission";
+import Transaction from "./transaction";
+import Leaderboard from "./leaderboard";
+import Tornament from "./tornamentPlayers";
+import TornamentPlayers from "./tornamentPlayers";
 
 export {
   Rtp,
@@ -36,10 +37,11 @@ export {
   BetPlaced,
   GameAccess,
   Permission,
+  SpinRecord,
   Transaction,
   ErrorMessage,
   Reward,
   Tornament,
   Leaderboard,
-  TornamentPlayers
-}
+  TornamentPlayers,
+};
