0b7caa674d79959d44fa3b6677e6d0a059b0f302
Onwordi Prince Alfred <princelini4@gmail.com>
2023-03-03 03:10:37 +0100

    added transactions to user service

--- src/usability/UserService.ts
+++ src/usability/UserService.ts
@@ -1,57 +1,59 @@
+import { ClientSession } from "mongoose";
 import { IUser } from "../../types";
-import { User } from '../models';
+import { User } from "../models";
 
 class UserService {
-    private _id = "";
-    private partnerId = "";
+  private _id = "";
+  private partnerId = "";
 
-    constructor(_id = '', partnerId = '') {
-        this.partnerId = partnerId;
-        this._id = _id;
-    }
+  constructor(_id = "", partnerId = "") {
+    this.partnerId = partnerId;
+    this._id = _id;
+  }
 
-    public async create(params: Partial<IUser>) {
-        try {
-            const user = new User({ ...params });
-            await user.save();
-            return user;
-        } catch (error: any) {
-            throw new Error(error);
-        }
+  public async create(params: Partial<IUser>, tr?: ClientSession) {
+    try {
+      const user = new User({ ...params });
+      await user.save({ ...(tr && { session: tr }) });
+      return user;
+    } catch (error: any) {
+      throw new Error(error);
     }
+  }
 
-    public async findOne() {
-        const user = await User
-            .findOne()
-            .where('_id')
-            .equals(this._id)
-            .catch((e: any) => { throw new Error(e) });
-        return user;
-    }
+  public async findOne() {
+    const user = await User.findOne()
+      .where("_id")
+      .equals(this._id)
+      .catch((e: any) => {
+        throw new Error(e);
+      });
+    return user;
+  }
 
-    public async findAll() {
-        const users = await User
-            .find()
-            .where(this.partnerId && "partnerId")
-            .equals(this.partnerId && this.partnerId)
-            .catch((e: any) => { throw new Error(e) });
-        return users;
-    }
+  public async findAll() {
+    const users = await User.find()
+      .where(this.partnerId && "partnerId")
+      .equals(this.partnerId && this.partnerId)
+      .catch((e: any) => {
+        throw new Error(e);
+      });
+    return users;
+  }
 
-    public async updateOne(params: Partial<IUser>) {
-        const user = await User
-            .findOneAndUpdate(
-                { _id: this._id },
-                { ...params },
-                { new: true }
-            );
-        return user;
-    }
+  public async updateOne(params: Partial<IUser>, tr?: ClientSession) {
+    const user = await User.findOneAndUpdate(
+      { _id: this._id },
+      { ...params },
+      { new: true, ...(tr && { session: tr }) }
+    );
+    return user;
+  }
 
-    public async last() {
-        const user = await User.find().sort({_id:-1}).limit(1);
-        return user[0];
-    }
+  public async last() {
+    const user = await User.find().sort({ _id: -1 }).limit(1);
+    return user[0];
+  }
 }
 
 export default UserService;
